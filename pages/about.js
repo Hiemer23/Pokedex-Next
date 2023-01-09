dfb5297426a51b6948ce36c2cfc27560562683ea
Hiemer23 <andreyhiemer23@gmail.com>
2023-01-09 16:44:42 -0300

    problema com fetch

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+import styles from '../styles/About.module.css'
+
+export default function About() {
+    return (
+        <div className={styles.about}>
+            <h1>Sobre o site</h1>
+            <p>Aplicação do Curso de NextJs - Matheus Battisti</p>
+            <p>Projeto criado através do curso do Youtube do Canal <a className={styles.negrito} href='https://www.youtube.com/@MatheusBattisti' target='_blank'>Matheus Batisti</a></p>
+            <p>Link para a série no Youtube: <a className={styles.negrito} href='https://www.youtube.com/watch?v=XHrbg2iYNCg&list=PLnDvRpP8BnezfJcfiClWskFOLODeqI_Ft&index=1' target='_blank'>Curso de NextJS</a></p>
+            <Image src='/images/charizard.png' width="300" height="300" alt='Charizard'></Image>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
 import styles from '../styles/Home.module.css'
+import fetch from 'node-fetch'
+export const getStaticProps = async () => {
+  const maxPokemons = 251
+  const URL = 'https://jsonplaceholder.typicode.com/todos/'
+  let data
+  fetch(URL)
+    .then(res => res.json())
+    .then(data =>console.log(data))
+    .catch(e=>console.log(e))
+  //add pokemon index
+  // data.results.forEach((item, index) => {
+  //   item.id = index + 1
+  // })
+  console.log(data)
+  return {
+    props: {
+      data: data,
+    },
+  }
+}
+
 
 export default function Home() {
   return (
-    <h1>Opa</h1>
+    <div>
+      <h1>casa</h1>
+      {/* <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            {pokemon.name}
+          </li>
+        ))}
+      </ul> */}
+    </div>
   )
 }
