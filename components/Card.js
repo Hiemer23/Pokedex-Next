01fa17c33e224f808d7fc8908c6a0a954f1b40e7
Hiemer23 <andreyhiemer23@gmail.com>
2023-01-10 19:04:56 -0300

    Card e responsividade

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from '../styles/Card.module.css'
 
 export default function Card({ pokemon }) {
     return (
-        <div>
+        <div className={styles.card}>
             <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} width='120' height='120' alt={pokemon.name} />
-            <p>#{pokemon.id}</p>
-            <h3>{pokemon.name}</h3>
-            <Link href={`/pokemon/${pokemon.id}`}>Detalhes...</Link>
+            <p className={styles.id}>#{pokemon.id}</p>
+            <h3 className={styles.title}>{pokemon.name}</h3>
+            <Link legacyBehavior href={`/pokemon/${pokemon.id}`}><a className={styles.btn}>Detalhes</a></Link>
         </div>
     )
 }
