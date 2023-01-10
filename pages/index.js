c21984f236e1939434f6de82bfa45204a7651724
Hiemer23 <andreyhiemer23@gmail.com>
2023-01-09 21:14:47 -0300

    Adequação Pokemon from API

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,48 @@
-import styles from '../styles/Home.module.css'
-import fetch from 'node-fetch'
-export const getStaticProps = async () => {
-  const maxPokemons = 251
-  const URL = 'https://jsonplaceholder.typicode.com/todos/'
-  let data
-  fetch(URL)
-    .then(res => res.json())
-    .then(data =>console.log(data))
-    .catch(e=>console.log(e))
-  //add pokemon index
-  // data.results.forEach((item, index) => {
-  //   item.id = index + 1
-  // })
-  console.log(data)
+import Card from './components/Card'
+
+import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
+
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = `https://pokeapi.co/api/v2/pokemon`;
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+
+  const data = await res.json();
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
   return {
     props: {
-      data: data,
+      pokemons: data.results,
     },
-  }
+  };
 }
 
-
-export default function Home() {
+export default function Home({ pokemons }) {
   return (
-    <div>
-      <h1>casa</h1>
-      {/* <ul>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokeNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>
-            {pokemon.name}
-          </li>
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
-      </ul> */}
-    </div>
-  )
+      </div>
+    </>
+  );
 }
