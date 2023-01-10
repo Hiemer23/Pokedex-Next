856914f4f64525707570e5a007d6a7afaf8ff0c5
Hiemer23 <andreyhiemer23@gmail.com>
2023-01-10 13:08:25 -0300

    testes do fetch, problema na imagem

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function Card({ pokemon }) {
     return (
-        <div>
-            <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} width='120' height='120' alt={pokemon.name} />
+        <>
+             <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} width='120' height='120' alt={pokemon.name} />
             <p>#{pokemon.id}</p>
             <h3>{pokemon.name}</h3>
             <Link href={`/pokemon/${pokemon.id}`}>Detalhes...</Link>
-        </div>
+        </>
     )
 }
